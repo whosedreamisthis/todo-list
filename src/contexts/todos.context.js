1ba506b6c709ba70957b5bf8429405d7541d3b43
Dana Sharon <whosedreamisthis@gmail.com>
2025-07-12 10:27:51 -0700

    removed the hardcoded initial todos

--- src/contexts/todos.context.js
+++ src/contexts/todos.context.js
@@ -1,19 +1,15 @@
 import React, { createContext } from 'react';
 import todoReducer from '../reducers/todos.reducer.js';
 import useLocalStorageReducer from '../hooks/useLocalStorageReducer.js';
-const defaultTodos = [
-	{ id: 1, task: 'Learn Hooks', completed: false },
-	{ id: 2, task: 'Build a Todo App', completed: false },
-];
+// const defaultTodos = [
+// 	{ id: 1, task: 'Learn Hooks', completed: false },
+// 	{ id: 2, task: 'Build a Todo App', completed: false },
+// ];
 export const TodosContext = createContext();
 export const DispatchContext = createContext();
 
 export function TodosProvider({ children }) {
-	const [todos, dispatch] = useLocalStorageReducer(
-		'todos',
-		defaultTodos,
-		todoReducer
-	);
+	const [todos, dispatch] = useLocalStorageReducer('todos', [], todoReducer);
 
 	return (
 		<TodosContext.Provider value={todos}>
